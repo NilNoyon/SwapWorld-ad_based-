00042863e4281fc806247a9391cf2c40ced30595
Noyon <noyon13nil@gmail.com>
2019-09-06 02:17:31 +0600

    all pushed in one commit

---
+++ swapworld/resources/views/mail.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+	<title>Reset Password</title>
+
+</head>
+
+<body>
+
+	<h1>Mail from swap team.</h1>
+
+	<p>Dear user, your news password is :{{ $pswrd }}</p>
+	<p>After login you can change or update your password.Please keep your password safe.</p>
+
+	<img src="/{{$image}}" alt="Image_not_found">
+	<p>Thank You </p>
+
+</body>
+
+</html>
